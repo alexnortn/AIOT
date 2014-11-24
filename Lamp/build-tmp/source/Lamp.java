7ef2a68322426680e221ef1fa5c455eb01be99fb
alexnortn <alexnortn@gmail.com>
2014-11-23 22:41:56 -0500

    adding future parsing

--- Lamp/build-tmp/source/Lamp.java
+++ Lamp/build-tmp/source/Lamp.java
@@ -1,3 +1,21 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import processing.serial.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Lamp extends PApplet {
+
 
 
 
@@ -17,7 +35,7 @@
 
 
 
-import processing.serial.*;
+
 
 String weatherURL = "http://weather.yahooapis.com/forecastrss?w=1118370&u=f&d=7",
 	   boston = "2367105",
@@ -35,7 +53,7 @@ int    delayQuery,
 	   frameSpeed;
 
 
-void setup() {
+public void setup() {
 	size(200,200);
 	// delay is the time between API calls for weather updates
 	// multiple framerate by 60 (seconds)
@@ -54,7 +72,7 @@ void setup() {
 	// lampCom.write(weatherUpdate);  
 }
 
-void draw() {
+public void draw() {
 	frameRate(frameSpeed);
 	m = frameCount;
 	 if ((lampCom.available() > 0) || (m > 0)) {
@@ -67,31 +85,25 @@ void draw() {
 
 }
 
-char weatherQuery() {
+public char weatherQuery() {
 	report = loadXML(weatherURL);
 
 	// Do all the variable passing for the recent weather query here
 
 	  String location = report.getChild("channel/yweather:location").getString("city");
-	  int    windSpeed= report.getChild("channel/yweather:wind").getInt("speed");
-	  int    windDirection = report.getChild("channel/yweather:wind").getInt("direction");
+	  int windSpeed= report.getChild("channel/yweather:wind").getInt("speed");
+	  int windDirection = report.getChild("channel/yweather:wind").getInt("direction");
 
-	  XML    kid = report.getChild("channel/item");
+	  XML kid = report.getChild("channel/item");
 
-	  int    temperature = kid.getChild("yweather:condition").getInt("temp");
+	  int temperature = kid.getChild("yweather:condition").getInt("temp");
 	  String conditionText = kid.getChild("yweather:condition").getString("text");
-	  int    conditionCode = kid.getChild("yweather:condition").getInt("code");
+	  int conditionCode = kid.getChild("yweather:condition").getInt("code");
 	  String date = kid.getChild("yweather:condition").getString("date");
 
-	  	 // Forecasting Tomorrow + Weekend
  	  XML[] yweatherForecast = kid.getChildren("yweather:forecast");
 			println("This is the weather for tomorrow");
 			println(yweatherForecast[0]);
-	  int   tomWeather = yweatherForecast[0].getInt("code");
-
-	  
-
-
 
 
 	  String[] dateList = splitTokens(date); //create date string
@@ -151,10 +163,19 @@ char weatherQuery() {
 
 }
 
-void keyReleased() {
+public void keyReleased() {
 	if ((key == 'S') || ( key == 's')) {
 		weatherQuery();
 		weatherUpdate = weatherQuery();
 			lampCom.write(weatherUpdate);
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Lamp" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
